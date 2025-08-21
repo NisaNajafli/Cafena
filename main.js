01efd44c5f7636018e748129ea9cf9744510c4ba
NisaNajafli <ncfliniss@gmail.com>
2025-08-21 18:38:16 +0400

    new

---
+++ main.js
@@ -0,0 +1,9 @@
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("nav");
+  if (window.scrollY > 0) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});

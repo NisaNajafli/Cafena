82e37c95f9261c1f83cb305b410070ef859b8632
Nisa Najafli <ncfliniss@gmail.com>
2025-08-25 02:20:53 +0400

    final

--- main.js
+++ main.js
@@ -7,3 +7,36 @@ window.addEventListener("scroll", function () {
     header.classList.remove("scrolled");
   }
 });
+var swiper = new Swiper(".card_slider-review", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        speed: 1000,
+        autoplay: {
+            delay: 2000,
+        },
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        480: {
+            slidesPerView: 1,
+         },
+        768: {
+            slidesPerView: 1,
+        },
+        1024: {
+            slidesPerView: 1,
+        },
+        280: {
+          slidesPerView: 1,
+        },
+    }
+});
